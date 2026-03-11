3c8befb756435aa33add8a5229e97fe03bec3714
Lucas Rios <lucasfaesa@gmail.com>
2026-03-11 18:40:41 -0300

    initial setup, window creation

---
+++ src/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0,0,width, height);
+}
+
+
+int main() {
+    glfwInit();
+
+    //setting the opengl being used as ver. 3.3
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+    //setting the opengl profile as Core
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0,0, 800,600);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window); //swap drawing 2d images
+        glfwPollEvents(); //checks if any events are trigger (like keyboard input or mouse)
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
