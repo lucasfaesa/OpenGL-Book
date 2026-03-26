e5e20057d259d7491dc0934c2e686b8e32ea0aa2
Lucas Rios <lucasfaesa@gmail.com>
2026-03-25 21:20:12 -0300

    vertex shader, fragment shader, and linking them

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const std::string vertex_shader_source = R"(
+        #version 330 core
+        layout (location = 0) in vec3 aPos;
+        void main()
+        {
+            gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+        }
+    )";
+
+const std::string fragment_shader_source = R"(
+        #version 330 core
+        out vec4 FragColor;
+        void main()
+        {
+            FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+        }
+    )";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0,0,width, height);
 }
 
+void process_input(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
 
 int main() {
     glfwInit();
@@ -39,11 +64,53 @@ int main() {
 
     while (!glfwWindowShouldClose(window))
     {
-        glfwSwapBuffers(window); //swap drawing 2d images
+        process_input(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
         glfwPollEvents(); //checks if any events are trigger (like keyboard input or mouse)
+        glfwSwapBuffers(window); //swap drawing 2d images
     }
 
     glfwTerminate();
     return 0;
+
+
+    float vertices[] ={
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    unsigned int VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    const char* vertex_shader_code = vertex_shader_source.c_str();
+    glShaderSource(vertexShader, 1, &vertex_shader_code, NULL);
+    glCompileShader(vertexShader);
+
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    const char* fragment_shader_code = fragment_shader_source.c_str();
+    glShaderSource(fragmentShader, 1, &fragment_shader_code, NULL);
+    glCompileShader(fragmentShader);
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glUseProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
 }
 
